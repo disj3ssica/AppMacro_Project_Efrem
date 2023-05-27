5dbbed804c281906fddb88f94eedc666630985e9
Jessica Cremonese <92398325+disj3ssica@users.noreply.github.com>
2023-05-27 17:13:33 +0200

    new file:   Graphs/IRF 68 conf bands (lilla).svg 	new file:   Graphs/IRF 95 conf bands (lilla).svg 	new file:   Graphs/SerieStoricaVars.svg 	modified:   JessicaCremonese_AM_Project.aux 	modified:   JessicaCremonese_AM_Project.log 	modified:   JessicaCremonese_AM_Project.pdf 	modified:   JessicaCremonese_AM_Project.synctex.gz 	modified:   JessicaCremonese_AM_Project.tex 	deleted:    Project_Code.m 	modified:   database_mpshocks_infoshocks.xlsx 	deleted:    draft_MACRO PROJECT DEFINITIVE CODE.m 	deleted:    draft_definitive_code_AM_efrem.asv 	modified:   draft_definitive_code_AM_efrem.m

--- draft_definitive_code_AM_efrem.m
+++ draft_definitive_code_AM_efrem.m
@@ -3,7 +3,7 @@ clear; clear session; close all; clc
 % add toolbox path:
 addpath(genpath('C:\Users\Jessica\Desktop\AppMacro_Project_Efrem\VAR toolbox CesaBianchi\VAR-Toolbox-main\v3dot0'))
 
-%% 1. LOAD & STORE DATA
+%% 1.   LOAD & STORE DATA
 
 [xlsdata, xlstext] = xlsread('database_mpshocks_infoshocks.xlsx');
 dateStrings = xlstext(2:end, 1);
@@ -12,7 +12,7 @@ datesnum = Date2Num(dateStrings);
 %dateCellArray = cellstr(dates);     % store dates as a 'cell' type and not a 'datetime' type, to count them later
 vnames = xlstext(1, 2:end);    % Retrieve the variable names
 
-nvar    = length(vnames);  % num of vars
+nvar    = length(vnames);       % num of vars
 nobs    = size(dates,1);        % num of observ
 
 data   = Num2NaN(xlsdata);
@@ -30,19 +30,82 @@ FigSize(50,10)   % FigSize(xdim,ydim)
 
 for ii=1:nvar
     subplot(4,2,ii) % SUBPLOT(m,n,p) divides the current figure into an m-by-n grid
-    H(ii) = plot(DATA.(vnames{ii}),'LineWidth',3,'Color',cmap(1));
+    H(ii) = plot(DATA.(vnames{ii}),'m-', 'LineWidth',1.5);
     title(vnames(ii)); 
     DatesPlot(datesnum(1),nobs,6,'m') % Set the x-axis label dates
     grid on; 
 end
-%SaveFigure('graphics/SW_DATA',1)
+% save figure manually as VECTOR IMAGE (max possible quality)
 %clf('reset')
 
-%% 3. VAR ESTIMATION
+
+
+
+%% 3.   VAR ESTIMATION  [   USING 4. IDENTIFICATION WITH ZERO CONTEMPORANEOUS RESTRICTIONS  ]
+
+% 3.1   VAR ESTIMATION
 %************************************************************************** 
 % VAR estimations is achieved in two steps: 
 %       (1) define the endogenous variables, the desired number of lags, and deterministic variables; 
 %       (2) run the VARmodel function.
+    %========================================================================
+    % [VAR, VARopt] = VARmodel(ENDO,nlag,const,EXOG,nlag_ex)
+    % -----------------------------------------------------------------------
+    % INPUT
+    %	- ENDO: an (nobs x nvar) matrix of y-vectors
+    %	- nlag: lag length
+    % -----------------------------------------------------------------------
+    % OPTIONAL INPUT
+    %	- const: 0 no constant; 1 constant; 2 constant and trend; 3 constant 
+    %       and trend^2 [dflt = 0]
+    %	- EXOG: optional matrix of variables (nobs x nvar_ex)
+    %	- nlag_ex: number of lags for exogeonus variables [dflt = 0]
+    % -----------------------------------------------------------------------
+    % OUTPUT
+    %   - VAR: structure including VAR estimation results
+    %   - VARopt: structure including VAR options (see VARoption)
+    % -----------------------------------------------------------------------
+
+% Select ENDO variables [ TO CHECK ]
+Xvnames  = {'INFO_FF4','Unempl_Rate','logCPI100','interest_rate_3month','spread_10yr3month','GFC'};
+Xvnames_long = {'Information shock proxy','Unemployment Rate', 'LogPrices','Policy Rate', 'Spread', 'Global Financial Cycle'};
+Xnvar = length(Xvnames);
+
+% Matrices [ TO CHECK ]
+X = nan(nobs, Xnvar)
+for ii = 1 : Xnvar
+    X(: , ii) = DATA.(Xvnames{ii});
+end
+
+% VAR parameters
+det = 1;
+nlags = 3;
+
+% Estimate VAR
+[VAR, VARopt] = VARmodel(X, nlags, det);
+
+% Update the VARopt structure (edit VARoptions to see meaning)
+VARopt.vnames = Xvnames_long;   % endogenous variables names
+VARopt.nsteps = 24;             % number of steps for computation of IRFs and FEVDs
+VARopt.quality = 1;             % quality of exported figures: 1=high (ghostscript required), 0=low
+VARopt.FigSize = [26,12];       % size of window for plots
+VARopt.firstdate = datesnum(1); % initial date of the sample in format 1999.75 => 1999Q4 (both for annual and quarterly data)
+VARopt.frequency = 'm';         % frequency of the data: 'm' monthly, 'q' quarterly, 'y' yearly
+VARopt.figname= 'graphics/SW_'; % string for name of exported figure
+
+VARopt.pctg      = 68;          % confidence level for bootstrap
+
+% 3.2   IMPULSE RESPONSES
+%-------------------------------------------------------------------------- 
+% To get zero contemporaneous restrictions set
+VARopt.ident = 'short'; % identification method for IRFs ('short' zero short-run restr [CHOLESKY], 'long' zero long-run restr, 'sign' sign restr, 'iv' external instrument)
+VARopt.snames = {'\epsilon^{1}','\epsilon^{2}','\epsilon^{InfShock}'};    % shocks names
+% Compute IR
+[IR, VAR] = VARir(VAR,VARopt);
+% Compute IR error bands
+[IRinf,IRsup,IRmed,IRbar] = VARirband(VAR,VARopt);
+% Plot IR
+VARirplot(IRbar,VARopt,IRinf,IRsup);
 
 
 
